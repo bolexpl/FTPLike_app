eb584b2728a2da8423aec53299f565798b785ef1
bolek <arek2159@gmail.com>
2018-06-25 22:25:38 +0200

    progress updating

--- FTPServer/src/com/company/clients/ClientThread.java
+++ FTPServer/src/com/company/clients/ClientThread.java
@@ -53,9 +53,7 @@ public class ClientThread extends Thread {
 
         reader = new BufferedReader(new InputStreamReader(controlSocket.getInputStream()));
         writer = new BufferedWriter(new OutputStreamWriter(controlSocket.getOutputStream()));
-//        explorer = new LocalExplorer(System.getProperty("user.home"));
-//        TODO
-        explorer = new LocalExplorer("/home/bolek/test/serv");
+        explorer = new LocalExplorer(System.getProperty("user.home"));
         start();
     }
 
